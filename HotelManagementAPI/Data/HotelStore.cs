3cb8de1d0bae9ccc79a256503037e65730e931c9
Ellie <yokomone@yahoo.com>
2024-05-16 11:13:52 +0300

    Created HotelStore

---
+++ HotelManagementAPI/Data/HotelStore.cs
@@ -0,0 +1,20 @@
+﻿using HotelManagementAPI.Models;
+using HotelManagementAPI.Models.DTO;
+using System.Runtime.CompilerServices;
+
+namespace HotelManagementAPI.Data
+{
+    public class HotelStore
+    {
+        public static HotelManagementContext context = new HotelManagementContext();
+        public static IEnumerable<HotelDTO> allHotels = from hotel in context.Hotels
+                                                        select new HotelDTO()
+                                                        {
+                                                            Id = hotel.Id,
+                                                            Name = hotel.Name,
+                                                            CurrencyFormat = hotel.Currency.FormattingString,
+                                                            DownPaymentPercentage = hotel.DownPaymentPercentage
+                                                        };
+
+    }
+}
