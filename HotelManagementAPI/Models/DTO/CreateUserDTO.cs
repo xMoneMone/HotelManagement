88f9eca5a4d72a503e9092c2e885b281e174c270
Ellie <yokomone@yahoo.com>
2024-05-16 13:50:08 +0300

    Created UserDTO

---
+++ HotelManagementAPI/Models/DTO/CreateUserDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelManagementAPI.Models.DTO
+{
+    public class CreateUserDTO
+    {
+        public int ColorId { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+    }
+}
