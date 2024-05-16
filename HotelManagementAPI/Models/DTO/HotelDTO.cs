5bdd58139761c355e6474177ba0cce39bffd4acf
Ellie <yokomone@yahoo.com>
2024-05-16 11:13:38 +0300

    Created HotelDTO

---
+++ HotelManagementAPI/Models/DTO/HotelDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelManagementAPI.Models.DTO
+{
+    public class HotelDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? CurrencyFormat { get; set; }
+
+        public int? DownPaymentPercentage { get; set; }
+    }
+}
