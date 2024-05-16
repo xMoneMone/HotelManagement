a30ec096f57dc84e8a5ccef4eb89ba1a20d0f22a
Ellie <yokomone@yahoo.com>
2024-05-16 13:50:18 +0300

    Created UserStore

---
+++ HotelManagementAPI/Data/UserStore.cs
@@ -0,0 +1,10 @@
+﻿using HotelManagementAPI.Models;
+using HotelManagementAPI.Models.DTO;
+
+namespace HotelManagementAPI.Data
+{
+    public class UserStore
+    {
+        public static HotelManagementContext context = new HotelManagementContext();
+    }
+}
