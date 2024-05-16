3018f8abed769df5f4c89d5c2d3ff447d0fe97b4
Ellie <yokomone@yahoo.com>
2024-05-16 16:14:37 +0300

    Added delete user

--- HotelManagementAPI/Controllers/UsersController.cs
+++ HotelManagementAPI/Controllers/UsersController.cs
@@ -36,5 +36,29 @@ namespace HotelManagementAPI.Controllers
             UserStore.context.SaveChanges();
             return Ok(userDTO);
         }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteUser(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var user = UserStore.context.Users.FirstOrDefault(x => x.Id == id);
+            Console.WriteLine(user);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            UserStore.context.Users.Remove(user);
+            UserStore.context.SaveChanges();
+            return Ok();
+        }
     }
 }
