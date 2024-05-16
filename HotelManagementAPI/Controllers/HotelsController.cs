0428b3e7eaa1586135a94afcefc65596324e2ee4
Ellie <yokomone@yahoo.com>
2024-05-16 11:14:53 +0300

    Created HotelsController

---
+++ HotelManagementAPI/Controllers/HotelsController.cs
@@ -0,0 +1,18 @@
+﻿using HotelManagementAPI.Data;
+using HotelManagementAPI.Models;
+using HotelManagementAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementAPI.Controllers
+{
+    [Route("api/hotels")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<HotelDTO> GetHotels()
+        {
+            return HotelStore.allHotels;
+        }
+    }
+}   
