54140f3fe20717c3b9ce60e76715197076fb408e
Ellie <yokomone@yahoo.com>
2024-05-17 21:20:12 +0300

    Created a ColourValidatior function

---
+++ HotelManagementAPI/Util/Validators.cs
@@ -0,0 +1,18 @@
+﻿using HotelManagementAPI.Data;
+
+namespace HotelManagementAPI.Util
+{
+    public class Validators
+    {
+        public static int ColorValidator(int colorId)
+        {
+            int[] colors = UserStore.context.Colors.Select(x => x.Id).ToArray();
+            if (!colors.Contains(colorId))
+            {
+                return colors[0];
+            }
+
+            return colorId;
+        }
+    }
+}
