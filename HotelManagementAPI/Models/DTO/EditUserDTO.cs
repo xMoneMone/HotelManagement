721e1051725d4be522182ed660403b18ddf62e17
Ellie <yokomone@yahoo.com>
2024-05-17 21:20:25 +0300

    Created EditUserDTO

---
+++ HotelManagementAPI/Models/DTO/EditUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelManagementAPI.Models.DTO
+{
+    public class EditUserDTO
+    {
+        public int ColorId { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+    }
+}
