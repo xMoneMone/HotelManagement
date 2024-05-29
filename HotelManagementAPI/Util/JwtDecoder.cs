a92d716731c8307a5a42f11c7668f0d3aab22af3
Ellie <yokomone@yahoo.com>
2024-05-29 14:19:44 +0300

    Added JwtDecoder.GetUser

---
+++ HotelManagementAPI/Util/JwtDecoder.cs
@@ -0,0 +1,14 @@
+﻿using HotelManagementAPI.Models;
+using System.Security.Claims;
+
+namespace HotelManagementAPI.Util
+{
+    public class JwtDecoder
+    {
+        public static User GetUser(IEnumerable<Claim> claims, HotelManagementContext context)
+        {
+            int userId = int.Parse(claims.First(i => i.Type == ClaimTypes.NameIdentifier).Value);
+            return context.Users.FirstOrDefault(x => x.Id == userId);
+        }
+    }
+}
