9d02caecef781146f2a05cf74d39bfb0d298df7d
Ellie <yokomone@yahoo.com>
2024-05-31 14:15:34 +0300

    Added AcceptInvite endpoint

--- HotelManagementAPI/Controllers/InvitationsController.cs
+++ HotelManagementAPI/Controllers/InvitationsController.cs
@@ -34,14 +34,52 @@ namespace HotelManagementAPI.Controllers
             string code = CodeGenerator.GenerateCode();
             var hotelCode = new HotelCode
             {
-                UserId = user.Id,
+                UserId = employee.Id,
                 Code = code,
-                HotelId = HotelCodeDTO.HotelId
+                HotelId = HotelCodeDTO.HotelId,
+                StatusId = 1
             };
 
             HotelStore.context.HotelCodes.Add(hotelCode);
             HotelStore.context.SaveChanges();
             return Ok(code);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [HttpPost("{codeId}"), Authorize]
+        public IActionResult AcceptInvitation(string codeId)
+        {
+            var code = HotelStore.context.HotelCodes.FirstOrDefault(x => x.Code == codeId);
+            var user = JwtDecoder.GetUser(User.Claims, UserStore.context);
+
+            if (code == null)
+            {
+                return BadRequest("Code does not exist.");
+            }
+
+            if (user.Id != code.UserId)
+            {
+                return Unauthorized("You are not the recepient of this invite.");
+            }
+
+            if (code.StatusId != 1)
+            {
+                return BadRequest("Code already used.");
+            }
+
+            code.StatusId = 2;
+            var hotelEmployeeConnection = new UsersHotel
+            {
+                HotelId = code.HotelId,
+                UserId = code.UserId
+            };
+
+            HotelStore.context.UsersHotels.Add(hotelEmployeeConnection);
+            HotelStore.context.SaveChanges();
+
+            return Ok("Employee added to hotel.");
         }
     }
 }
