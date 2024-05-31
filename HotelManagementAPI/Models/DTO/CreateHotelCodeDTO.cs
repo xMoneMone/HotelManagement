59c5848d46c9a3d60ae8e9f3f931a1b7b31a0602
Ellie <yokomone@yahoo.com>
2024-05-31 11:25:32 +0300

    Created CreateHotelCodeDTO

---
+++ HotelManagementAPI/Models/DTO/CreateHotelCodeDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelManagementAPI.Models.DTO
+{
+    public class CreateHotelCodeDTO
+    {
+
+        public int? HotelId { get; set; }
+
+        public string UserEmail { get; set; }
+    }
+}
