0119fafbab99f7670307a626b72670f753ff5d71
Ellie <yokomone@yahoo.com>
2024-06-01 20:59:40 +0300

    Create HotelCodeSentDTO.cs

---
+++ HotelManagementAPI/Models/DTO/HotelCodeSentDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelManagementAPI.Models.DTO
+{
+    public class HotelCodeSentDTO
+    {
+        public string HotelName { get; set; } = null!;
+
+        public string UserEmail { get; set; } = null!;
+
+        public string Status { get; set; } = null!;
+    }
+}
