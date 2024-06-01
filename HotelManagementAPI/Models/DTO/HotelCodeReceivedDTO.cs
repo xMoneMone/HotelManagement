b8b5d821ab4f505a49dd2a86733b1e03b83bae05
Ellie <yokomone@yahoo.com>
2024-06-01 20:59:46 +0300

    Create HotelCodeReceivedDTO.cs

---
+++ HotelManagementAPI/Models/DTO/HotelCodeReceivedDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelManagementAPI.Models.DTO
+{
+    public class HotelCodeReceivedDTO
+    {
+        public string HotelName { get; set; } = null!;
+
+        public string OwnerEmail { get; set;} = null!;
+    }
+}
