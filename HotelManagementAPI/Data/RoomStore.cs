82d72a04b4a90340334363f3643984e041e6ba3a
Ellie <yokomone@yahoo.com>
2024-06-02 19:01:48 +0300

    Created RoomStore GetRooms

---
+++ HotelManagementAPI/Data/RoomStore.cs
@@ -0,0 +1,23 @@
+﻿using HotelManagementAPI.Models;
+using HotelManagementAPI.Models.DTO;
+
+namespace HotelManagementAPI.Data
+{
+    public class RoomStore
+    {
+        public static HotelManagementContext context = new HotelManagementContext();
+
+        public static IEnumerable<RoomDTO> GetRooms(int hotelId)
+        {
+            return from room in context.Rooms
+                   where room.HotelId == hotelId
+                   select new RoomDTO
+                   {
+                       Id = room.Id,
+                       RoomNumber = room.RoomNumber,
+                       PricePerNight = room.PricePerNight,
+                       Notes = room.Notes
+                   };
+        }
+    }
+}
