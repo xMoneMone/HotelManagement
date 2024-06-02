97401df3c6566a6cca98457f1ac96b0f8b95787d
Ellie <yokomone@yahoo.com>
2024-06-02 19:02:35 +0300

    Created GetRooms endpoint

---
+++ HotelManagementAPI/Controllers/RoomsController.cs
@@ -0,0 +1,29 @@
+﻿using HotelManagementAPI.Data;
+using HotelManagementAPI.Models;
+using HotelManagementAPI.Models.DTO;
+using HotelManagementAPI.Util;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementAPI.Controllers
+{
+    [Route("rooms"), Authorize]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet("{hotelId:int}"), Authorize]
+        public IActionResult GetRooms(int hotelId)
+        {
+            var user = JwtDecoder.GetUser(User.Claims, UserStore.context);
+
+            var error = RoomValidators.GetRoomsValidator(user, hotelId);
+
+            if (error != null)
+            {
+                return error;
+            }
+
+            return Ok(RoomStore.GetRooms(hotelId));
+        }
+    }
+}
