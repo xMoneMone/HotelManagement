77696d769d1e09a7216eaf26d917453d7a5b20c8
Ellie <yokomone@yahoo.com>
2024-06-03 10:35:40 +0300

    Created CreateRoom endpoint

--- HotelManagementAPI/Controllers/RoomsController.cs
+++ HotelManagementAPI/Controllers/RoomsController.cs
@@ -12,6 +12,9 @@ namespace HotelManagementAPI.Controllers
     public class RoomsController : ControllerBase
     {
         [HttpGet("{hotelId:int}"), Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public IActionResult GetRooms(int hotelId)
         {
             var user = JwtDecoder.GetUser(User.Claims, UserStore.context);
@@ -24,6 +27,35 @@ namespace HotelManagementAPI.Controllers
             }
 
             return Ok(RoomStore.GetRooms(hotelId));
+        }
+
+        [HttpPost, Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult CreateRoom([FromBody] RoomCreateDTO roomDTO)
+        {
+            var user = JwtDecoder.GetUser(User.Claims, UserStore.context);
+
+            var error = RoomValidators.CreateRoomValidator(user, roomDTO);
+
+            if (error != null)
+            {
+                return error;
+            }
+
+            var room = new Room
+            {
+                RoomNumber = roomDTO.RoomNumber,
+                PricePerNight = roomDTO.PricePerNight,
+                Notes = roomDTO.Notes,
+                HotelId = roomDTO.HotelId
+            };
+
+            RoomStore.context.Rooms.Add(room);
+            RoomStore.context.SaveChanges();
+
+            return Ok("Room created successfully.");
         }
     }
 }
