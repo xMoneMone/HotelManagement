d94ac36ee08da48d7f1a41ca48530c45e31f890a
Ellie <yokomone@yahoo.com>
2024-06-03 14:47:20 +0300

    Created GetBookingsValidator

---
+++ HotelManagementAPI/Util/BookingValidators.cs
@@ -0,0 +1,27 @@
+﻿using HotelManagementAPI.Data;
+using HotelManagementAPI.Models;
+using HotelManagementAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementAPI.Util
+{
+    public class BookingValidators
+    {
+        public static IActionResult? GetBookingsValidator(User user, Room room)
+        {
+            if (room == null)
+            {
+                return new BadRequestObjectResult("Room does not exist.");
+            }
+
+            var hotel = HotelStore.context.Hotels.FirstOrDefault(x => x.Id == room.HotelId);
+
+            if (hotel.OwnerId != user.Id && Validators.EmployeeWorksAtHotel(hotel.Id, user.Id))
+            {
+                return new UnauthorizedObjectResult("You do not have permission to see this resource.");
+            }
+
+            return null;
+        }
+    }
+}
