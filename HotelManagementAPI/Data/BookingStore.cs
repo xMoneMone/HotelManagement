70d33a41ef0af8c5eb697bb8ecb0a790cbe29857
Ellie <yokomone@yahoo.com>
2024-06-03 14:46:42 +0300

    Created BookingStore

---
+++ HotelManagementAPI/Data/BookingStore.cs
@@ -0,0 +1,30 @@
+﻿using HotelManagementAPI.Models;
+using HotelManagementAPI.Models.DTO;
+
+namespace HotelManagementAPI.Data
+{
+    public class BookingStore
+    {
+        public static HotelManagementContext context = new HotelManagementContext();
+
+        public static IEnumerable<BookingDTO> GetBookings(Room room)
+        {
+            return from booking in context.Bookings
+                   where booking.RoomId == room.Id
+                   select new BookingDTO
+                   {
+                       Id = booking.Id,
+                       FirstName = booking.FirstName,
+                       LastName = booking.LastName,
+                       StartDate = booking.StartDate,
+                       EndDate = booking.EndDate,
+                       DownPaymentPaid = booking.DownPaymentPaid,
+                       FullPaymentPaid = booking.FullPaymentPaid,
+                       DownPaymentPrice = booking.DownPaymentPrice,
+                       FullPaymentPrice = booking.FullPaymentPrice,
+                       Notes = booking.Notes,
+                       RoomId = room.Id
+                   };
+        }
+    }
+}
