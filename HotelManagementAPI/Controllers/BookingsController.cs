d757b2fc5313083f296c7bb583ea1f79813d6286
Ellie <yokomone@yahoo.com>
2024-06-03 14:49:16 +0300

    Added CreateBooking endpoint

--- HotelManagementAPI/Controllers/BookingsController.cs
+++ HotelManagementAPI/Controllers/BookingsController.cs
@@ -29,5 +29,40 @@ namespace HotelManagementAPI.Controllers
 
             return Ok(BookingStore.GetBookings(room));
         }
+
+        [HttpPost("{roomId:int}"), Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult CreateBooking([FromBody] CreateBookingDTO bookingDTO, int roomId)
+        {
+            var user = JwtDecoder.GetUser(User.Claims, UserStore.context);
+
+            var error = BookingValidators.CreateBookingValidator(user, bookingDTO, roomId);
+
+            if (error != null)
+            {
+                return error;
+            }
+
+            var booking = new Booking
+            {
+                FirstName = bookingDTO.FirstName,
+                LastName = bookingDTO.LastName,
+                StartDate = bookingDTO.StartDate,
+                EndDate = bookingDTO.EndDate,
+                DownPaymentPaid = bookingDTO.DownPaymentPaid,
+                FullPaymentPaid = bookingDTO.FullPaymentPaid,
+                DownPaymentPrice = bookingDTO.DownPaymentPrice,
+                FullPaymentPrice = bookingDTO.FullPaymentPrice,
+                Notes = bookingDTO.Notes,
+                RoomId = roomId
+            };
+
+            BookingStore.context.Bookings.Add(booking);
+            BookingStore.context.SaveChanges();
+
+            return Ok("Booking created successfully.");
+        }
     }
 }
