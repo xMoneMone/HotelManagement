3b56a27de3698fd9341ff7e1bef4e5c85091cae3
Ellie <yokomone@yahoo.com>
2024-06-03 10:35:01 +0300

    Created RoomCreateDTO

---
+++ HotelManagementAPI/Models/DTO/RoomCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelManagementAPI.Models.DTO
+{
+    public class RoomCreateDTO
+    {
+        public string RoomNumber { get; set; } = null!;
+
+        public decimal PricePerNight { get; set; }
+
+        public string? Notes { get; set; }
+
+        public int HotelId { get; set; }
+    }
+}
