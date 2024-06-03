e66495b18acb103dd7cdc95e34f70d61891b48a0
Ellie <yokomone@yahoo.com>
2024-06-03 10:35:30 +0300

    Created CreateRoomValidator

--- HotelManagementAPI/Util/RoomValidators.cs
+++ HotelManagementAPI/Util/RoomValidators.cs
@@ -1,5 +1,6 @@
 ﻿using HotelManagementAPI.Data;
 using HotelManagementAPI.Models;
+using HotelManagementAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
@@ -12,7 +13,7 @@ namespace HotelManagementAPI.Util
             var hotel = HotelStore.context.Hotels.FirstOrDefault(x => x.Id == hotelId);
             var employeesAtHotel = HotelStore.GetHotelEmployeesIds(hotelId);
 
-            if (hotel ==  null)
+            if (hotel == null)
             {
                 return new BadRequestObjectResult("Hotel does not exist.");
             }
@@ -20,6 +21,38 @@ namespace HotelManagementAPI.Util
             if (hotel.OwnerId != user.Id && !employeesAtHotel.Contains(user.Id))
             {
                 return new UnauthorizedObjectResult("You do not have permission to see this resource.");
+            }
+
+            return null;
+        }
+
+        public static IActionResult? CreateRoomValidator(User user, RoomCreateDTO roomDTO)
+        {
+            var hotel = HotelStore.context.Hotels.FirstOrDefault(x => x.Id == roomDTO.HotelId);
+
+            if (hotel == null)
+            {
+                return new BadRequestObjectResult("Hotel does not exist.");
+            }
+
+            if (hotel.OwnerId != user.Id)
+            {
+                return new UnauthorizedObjectResult("You are not the owner of this hotel    .");
+            }
+
+            if (roomDTO.RoomNumber.Length <= 0 || roomDTO.RoomNumber.Length > 50)
+            {
+                return new BadRequestObjectResult("Room number must be below 50 characters.");
+            }
+
+            if (roomDTO.PricePerNight < 0)
+            {
+                return new BadRequestObjectResult("Room price cannot be a negative number.");
+            }
+
+            if (roomDTO.Notes != null && roomDTO.Notes.Length > 300)
+            {
+                return new BadRequestObjectResult("Room notes must not exceed 300 characters.");
             }
 
             return null;
