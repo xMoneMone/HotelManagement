9a19da67b1857aeadafec1ecaa821e40cae466ba
Ellie <yokomone@yahoo.com>
2024-06-03 14:48:00 +0300

    Created GetBookings endpoint

---
+++ HotelManagementAPI/Controllers/BookingsController.cs
@@ -0,0 +1,33 @@
+﻿using HotelManagementAPI.Data;
+using HotelManagementAPI.Models;
+using HotelManagementAPI.Models.DTO;
+using HotelManagementAPI.Util;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementAPI.Controllers
+{
+    [Route("bookings"), Authorize]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        [HttpGet("all-bookings/{roomId:int}"), Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult GetBookings(int roomId)
+        {
+            var user = JwtDecoder.GetUser(User.Claims, UserStore.context);
+            var room = RoomStore.context.Rooms.FirstOrDefault(x => x.Id == roomId);
+
+            var error = BookingValidators.GetBookingsValidator(user, room);
+
+            if (error != null)
+            {
+                return error;
+            }
+
+            return Ok(BookingStore.GetBookings(room));
+        }
+    }
+}
