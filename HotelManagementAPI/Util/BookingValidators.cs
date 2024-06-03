bf46614dce8f903cd64e8b9a8280c7137de06035
Ellie <yokomone@yahoo.com>
2024-06-03 14:49:01 +0300

    Added CreateBookingValidator

--- HotelManagementAPI/Util/BookingValidators.cs
+++ HotelManagementAPI/Util/BookingValidators.cs
@@ -23,5 +23,50 @@ namespace HotelManagementAPI.Util
 
             return null;
         }
+
+        public static IActionResult? CreateBookingValidator(User user, CreateBookingDTO bookingDTO, int roomId)
+        {
+            var room = RoomStore.context.Rooms.FirstOrDefault(x => x.Id == roomId);
+
+            if (room == null)
+            {
+                return new BadRequestObjectResult("Room does not exist.");
+            }
+
+            var hotel = HotelStore.context.Hotels.FirstOrDefault(x => x.Id == room.HotelId);
+
+            if (hotel.OwnerId != user.Id || Validators.EmployeeWorksAtHotel(hotel.Id, user.Id))
+            {
+                return new UnauthorizedObjectResult("You cannot make bookings at this hotel.");
+            }
+
+            if (bookingDTO.FirstName.Length <= 0 || bookingDTO.FirstName.Length > 50)
+            {
+                return new BadRequestObjectResult("First name number must be below 50 characters.");
+            }
+
+            if (bookingDTO.LastName.Length <= 0 || bookingDTO.LastName.Length > 50)
+            {
+                return new BadRequestObjectResult("First name number must be below 50 characters.");
+            }
+
+            if (bookingDTO.DownPaymentPrice < 0)
+            {
+                return new BadRequestObjectResult("Down payment price cannot be a negative number.");
+            }
+
+            if (bookingDTO.FullPaymentPrice < 0)
+            {
+                return new BadRequestObjectResult("Full payment price cannot be a negative number.");
+            }
+
+            if (bookingDTO.Notes != null && bookingDTO.Notes.Length > 300)
+            {
+                return new BadRequestObjectResult("Room notes must not exceed 300 characters.");
+            }
+
+            return null;
+        }
+
     }
 }
