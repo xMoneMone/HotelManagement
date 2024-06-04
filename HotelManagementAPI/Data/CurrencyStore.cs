13239701e3957acbb202387a4520480c045721e9
Ellie <yokomone@yahoo.com>
2024-06-05 00:30:41 +0300

    Create CurrencyStore.cs

---
+++ HotelManagementAPI/Data/CurrencyStore.cs
@@ -0,0 +1,15 @@
+﻿using HotelManagementAPI.Models;
+
+namespace HotelManagementAPI.Data
+{
+    public class CurrencyStore : DataStore
+    {
+        public static Currency? GetById(int id)
+        {
+            return (from currency in context.Currencies
+                    where id == currency.Id
+                    select currency)
+                   .FirstOrDefault();
+        }
+    }
+}
