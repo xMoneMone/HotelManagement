9b350051b4c978f90762e2d845c5d6223648f24b
Ellie <yokomone@yahoo.com>
2024-06-06 20:25:28 +0300

    Renamed JwtDecoder to Jwt

--- HotelManagementAPI/Util/Jwt.cs
+++ HotelManagementAPI/Util/Jwt.cs
@@ -4,12 +4,13 @@ using System.Security.Claims;
 
 namespace HotelManagementAPI.Util
 {
-    public class JwtDecoder
+    public class Jwt
     {
-        public static int GetUser(IEnumerable<Claim> claims)
+        public static int DecodeUser(IEnumerable<Claim> claims)
         {
             int userId = int.Parse(claims.First(i => i.Type == ClaimTypes.NameIdentifier).Value);
             return userId;
         }
+
     }
 }
