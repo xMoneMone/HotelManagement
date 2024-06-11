79b4bf9ce8a714e502cb9fecc205be889da17e7c
Ellie <yokomone@yahoo.com>
2024-06-11 13:11:09 +0300

    added user validators

--- HotelManagementAPI/Util/UserValidators.cs
+++ HotelManagementAPI/Util/UserValidators.cs
@@ -1,6 +1,7 @@
 ﻿using HotelManagementAPI.Models.DTO;
 using HotelManagementAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace HotelManagementAPI.Util
 {
@@ -8,9 +9,33 @@ namespace HotelManagementAPI.Util
     {
         public static IActionResult? CreateUserValidator(UserCreateDTO userDTO)
         {
+            Regex emailRegex = new(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            Regex passwordRegex = new(@"(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$");
+
             if (userDTO == null)
             {
                 return new BadRequestObjectResult(userDTO);
+            }
+
+            if (!emailRegex.IsMatch(userDTO.Email))
+            {
+                return new BadRequestObjectResult("Invalid email.");
+            }
+
+            if (!passwordRegex.IsMatch(userDTO.Password))
+            {
+                return new BadRequestObjectResult("Password must be at least 8 characters and must contain at least one uppercase letter," +
+                                                   " lowercase letter, number, and special character.");
+            }
+
+            if (userDTO.FirstName.Length <= 0 || userDTO.FirstName.Length > 50)
+            {
+                return new BadRequestObjectResult("First name number must be below 50 characters.");
+            }
+
+            if (userDTO.LastName.Length <= 0 || userDTO.LastName.Length > 50)
+            {
+                return new BadRequestObjectResult("First name number must be below 50 characters.");
             }
 
             return null;
@@ -33,6 +58,17 @@ namespace HotelManagementAPI.Util
 
         public static IActionResult? EditUserValidator(UserEditDTO userDTO)
         {
+
+            if (userDTO.FirstName.Length <= 0 || userDTO.FirstName.Length > 50)
+            {
+                return new BadRequestObjectResult("First name number must be below 50 characters.");
+            }
+
+            if (userDTO.LastName.Length <= 0 || userDTO.LastName.Length > 50)
+            {
+                return new BadRequestObjectResult("First name number must be below 50 characters.");
+            }
+
             return null;
         }
     }
