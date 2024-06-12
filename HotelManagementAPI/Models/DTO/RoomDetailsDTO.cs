f29ff89eb82fa32c6f1307a569833fbc01262c58
Ellie <yokomone@yahoo.com>
2024-06-12 11:28:17 +0300

    split off room dto into two

---
+++ HotelManagementAPI/Models/DTO/RoomDetailsDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelManagementAPI.Models.DTO
+{
+    public class RoomDetailsDTO
+    {
+        public int Id { get; set; }
+
+        public string RoomNumber { get; set; } = null!;
+
+        public decimal PricePerNight { get; set; }
+
+        public string? Notes { get; set; }
+
+        public virtual ICollection<BedDTO> Beds { get; set; } = new List<BedDTO>();
+    }
+}
---
+++ HotelManagementAPI/Models/DTO/RoomsDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelManagementAPI.Models.DTO
+{
+    public class RoomsDTO
+    {
+        public int Id { get; set; }
+
+        public string RoomNumber { get; set; } = null!;
+
+        public decimal PricePerNight { get; set; }
+
+        public string? Notes { get; set; }
+
+        public virtual int Capacity { get; set; }
+    }
+}
