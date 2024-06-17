6e1a81745318cff258ee45c79dc6046e3c771b77
Ellie <yokomone@yahoo.com>
2024-06-17 21:16:09 +0300

    added unauthorised home

--- HotelManagementFrontEnd/src/Home.jsx
+++ HotelManagementFrontEnd/src/Home.jsx
@@ -2,16 +2,19 @@ import Button from "./Button"
 import { useContext, useEffect } from "react"
 import { UserContext } from "./App"
 import { Link } from 'react-router-dom';
+import './css/home.css'
 
 function Home () {
+    const [token, setToken] = useContext(UserContext)
 
     useEffect(() => {
         document.title = 'Home | HMT';
       }, []);
       
     return  <>
+                {token && <div className="home-not-authorized">
                             
-                            <Link to=""><Button> SIGN UP</Button></Link>
+                            <Link to=""><Button>SIGN UP</Button></Link>
                             <Link to=""><Button>LOG IN</Button> </Link>
                             
                 </div>} 
