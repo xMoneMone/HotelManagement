3c6871ab8ace06658be85349d4e992d698cb31f4
Ellie <yokomone@yahoo.com>
2024-06-19 22:49:25 +0300

    added room filter

--- HotelManagementFrontEnd/src/Rooms.jsx
+++ HotelManagementFrontEnd/src/Rooms.jsx
@@ -1,25 +1,42 @@
 import { useParams } from "react-router-dom"
 import useRooms from "./hooks/useRooms"
 import useHotelCurrency from "./hooks/useHotelCurrency"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { UserContext } from "./App"
 import { Link } from 'react-router-dom';
 import Room from "./Room"
 import './css/room.css'
+import Button from "./Button"
 
 export default function Rooms() {
     const {pk} = useParams()
     const [token, setToken] = useContext(UserContext)
-    const {rooms} = useRooms(token, pk)
+    const [startDate, setStartDate] = useState(new Date().toISOString().slice(0, 10));
+    const [finishDate, setFinishDate] = useState(new Date().toISOString().slice(0, 10));
+    const {rooms, refresh} = useRooms(token, pk, "")
     const hotelCurrency = useHotelCurrency(token, pk)
+
+    function filterClick() {
+        refresh(`/${startDate}/${finishDate}`)
+    }
+
     return <>
             <div className="rooms">
+                <div className="date-selector">
+                    <input className="startDate"
+                           type="date" data-date-inline-picker="true"
+                           value={startDate}
+                           onChange={(e) => setStartDate(e.target.value)}/>
+                    <input className="finishDate" 
+                           type="date" data-date-inline-picker="true"
+                           value={finishDate}
+                           onChange={(e) => setFinishDate(e.target.value)}/>
+                    <Button onClick={filterClick}>FILTER</Button>
+                </div>
                 <div className="rooms-container">
-
                 {rooms.map((room) => {
-                    return <Link to="" key={room.id}><Room number={room.roomNumber} capacity={room.capacity} currency={hotelCurrency.currency} price={room.pricePerNight} available={room.available}></Room></Link>
+                    return <Link to={room.id.toString()} key={room.id}><Room number={room.roomNumber} capacity={room.capacity} currency={hotelCurrency.currency} price={room.pricePerNight} available={room.available}></Room></Link>
                 })}
-
                 </div>
             </div>
             </>
--- HotelManagementFrontEnd/src/hooks/useRooms.js
+++ HotelManagementFrontEnd/src/hooks/useRooms.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import { axios_base } from "../util/axios";
 
-export default (token, hotelId) => {
+export default (token, hotelId, dateRange) => {
     const [rooms, setRooms] = useState([]);
 
     useEffect(() => {
-        refresh()
+        refresh(dateRange)
       }, [])
 
-    const refresh = async () => {
-        const {data} = await axios_base.get(`hotels/${hotelId}/rooms`, {headers: {authorization: token}})
+    const refresh = async (date) => {
+        const {data} = await axios_base.get(`hotels/${hotelId}/rooms${date}`, {headers: {authorization: token}})
         setRooms(data)
     }
 
